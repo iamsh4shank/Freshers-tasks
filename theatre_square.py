2a8816ede8fe1db7d146509d5653b6f3e9f816a5
shashank629 <52889867+shashank629@users.noreply.github.com>
2019-08-07 00:38:00 +0530

    Add files via upload

---
+++ theatre_square.py
@@ -0,0 +1,16 @@
+a=raw_input()
+n=(float(a[0])*float(a[2]))/float(a[4])**2
+if n==int(n):
+    print int(n)
+if n!=int(n):
+    if (float(a[0])*float(a[2]))/float(a[4])**2 <1:
+        print int(n)+1
+    else:
+        print int(n)+2
+
+
+
+
+
+
+    
