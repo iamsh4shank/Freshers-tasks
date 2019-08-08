c0aaca04ef8bfb000229124cc2c83b0cffa7088e
shashank629 <52889867+shashank629@users.noreply.github.com>
2019-08-09 01:23:25 +0530

    Add files via upload

---
+++ Task-8.py
@@ -0,0 +1,8 @@
+a=raw_input().split()
+for i in range(len(a[0])):
+    c=raw_input().split()
+count=0
+for i in range(int(a[0])):
+    if int(c[i])>=int(c[int(a[1])-1]) and c[i]!='0':
+        count+=1    
+print count
