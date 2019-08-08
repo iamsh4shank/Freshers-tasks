ae7a28932039ba779a1f8c3e2e25a03381d1b862
shashank629 <52889867+shashank629@users.noreply.github.com>
2019-08-09 01:21:14 +0530

    Delete too_long.py

--- too_long.py
+++
@@ -1,9 +0,0 @@
-b=input()
-l=[]
-for i in range(b):
-    l.append(raw_input())
-for j in range(b):
-    if len(l[j])>10:
-        print l[j][0]+str(len(l[j])-2)+l[j][-1]
-    else:
-        print l[j]
