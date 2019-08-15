a7a5547ee1d4c313d5f13a6e0567920ff21a9ce1
shashank629 <52889867+shashank629@users.noreply.github.com>
2019-08-15 13:10:15 +0530

    Create compare_the _triplets.py

---
+++ TASK 2/Hackerrank/compare_the _triplets.py
@@ -0,0 +1,14 @@
+#Compare the triplets
+
+
+
+a=raw_input().split()
+b=raw_input().split()
+alices=0
+bobs=0
+for i in range(len(a)):
+    if int(a[i])>int(b[i]):
+        alices+=1
+    if int(a[i])<int(b[i]):
+        bobs+=1
+print alices,bobs
