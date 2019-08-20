9ff552e3f5c213fb24259c4689351e914888d45b
Shashank Priyadarshi <52889867+shashank629@users.noreply.github.com>
2019-08-21 03:59:45 +0530

    Update Diagonal_difference.py

--- TASK 2/Hackerrank/Diagonal_difference.py
+++ TASK 2/Hackerrank/Diagonal_difference.py
@@ -5,7 +5,7 @@ import sys
 
 N = int(raw_input())
 difference = 0
-for i in xrange(N):
+for i in range(N):
     row = raw_input().split()
     difference += (int(row[i]) - int(row[N-1-i]))
 print abs(difference)
