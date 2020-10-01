f70dc8995dbcec042c061de6bd79f1573a0af4bd
Anand Patel <72202462+anandpatel2506@users.noreply.github.com>
2020-10-01 20:57:23 +0530

    Update Way_to_long_words.py

--- Task-2/Codeforces/Way_to_long_words.py
+++ Task-2/Codeforces/Way_to_long_words.py
@@ -2,11 +2,11 @@
 #Way too long words
 
 
-b=input()
+a=input()
 l=[]
-for i in range(b):
+for i in range(a):
     l.append(raw_input())
-for j in range(b):
+for j in range(a):
     if len(l[j])>10:
         print l[j][0]+str(len(l[j])-2)+l[j][-1]
     else:
